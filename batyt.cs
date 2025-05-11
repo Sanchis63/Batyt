41522b64308583705e3cec9315f50aaa3cd7bf3d
Sanchis63 <sanchis63@mail.ru>
2025-05-11 18:36:33 +0400

    Add files via upload

--- batyt.cs
+++ batyt.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class batyt : RUN
 // RUN - контроллер игрока, в нем же и регистрируются столкновения с триггерами
 {
+public RUN rUN;
     new public void Update()
     {
-        RUN rUN = new RUN();
+        
         if (rUN.ToJump == true)
         {
             rUN.JunpForce = 350;
